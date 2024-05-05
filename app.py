85a546b0184fa0ccfdf34140125a2dd965c60ae3
Utkarsh Bhardwaj <151949177+ubhari@users.noreply.github.com>
2024-05-06 02:27:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def predict():
     return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
